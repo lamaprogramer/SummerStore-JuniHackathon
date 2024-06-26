cc2262cc57f2514eb4267914ea918b6877b89588
Iamaprogramer <willkinsey@tocomail.com>
2024-06-26 06:04:38 +0000

    Added ebay oauth api

--- src/main/java/net/iamaprogrammer/summerstore/Main.java
+++ src/main/java/net/iamaprogrammer/summerstore/Main.java
@@ -1,5 +1,10 @@
 package net.iamaprogrammer.summerstore;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javafx.application.Application; 
 import static javafx.application.Application.launch; 
 import javafx.geometry.Insets; 
@@ -19,11 +24,32 @@ import net.iamaprogrammer.summerstore.layers.BaseLayer;
 
 import com.google.gson.*;
 
-public class Main extends Application 
-{ 
+public class Main extends Application { 
+  private static final String EBAY_API_KEY = System.getenv("EBAY_API_KEY");
   
   @Override
   public void start(Stage stage) {
+
+    // try {
+    //   URL url = new URL("https://api.nal.usda.gov/fdc/v1/food/1750339?api_key=DEMO_KEY");
+    //   HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    //   conn.setRequestMethod("GET");
+    //   conn.setRequestProperty("Accept", "application/json");
+    //   if (conn.getResponseCode() != 200) {
+    //     throw new RuntimeException("Failed : HTTP Error code : "
+    //             + conn.getResponseCode());
+    //   }
+    //   InputStreamReader in = new InputStreamReader(conn.getInputStream());
+    //   BufferedReader br = new BufferedReader(in);
+    //   String output;
+    //   while ((output = br.readLine()) != null) {
+    //     System.out.println(output);
+    //   }
+    //   conn.disconnect();
+
+    // } catch (Exception e) {
+    //     System.out.println("Exception in NetClientGet:- " + e);
+    // }
 
     BaseLayer baseLayer = new BaseLayer();
 
@@ -46,7 +72,6 @@ public class Main extends Application
   } 
     
   public static void main(String[] args) {
-    System.out.println("Hello world!");
     launch(args);
   }
 } 
